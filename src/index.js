6ad9c914536f03eb3a99d7a75ab22b66132ccf05
puliwang@cxist.com <puliwang@cxist.com>
2022-06-07 16:46:48 +0800

    1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,36 @@ $(function () {
 });
 
 function funTest() {
-    console.log('test work!', Zone);
+
+    const taskId = `task ${+new Date()}`;
+    console.time(taskId);
+
+    const person = {
+        name: '小明',
+        age: 18,
+        eat: () => {
+            console.log('start eating');
+            setTimeout(() => {
+                console.log('eating finish');
+            }, 1500);
+        },
+        drink: () => {
+            console.log('start drinking');
+            setTimeout(() => {
+                console.log('drinking finish');
+            }, 1500);
+        }
+    };
+
+
+
+    const executeContext = () => {
+        person.eat();
+        person.drink();
+    };
+
+
+    // const myZone = Zone.fork
+    console.log('title:',zone);
+    console.timeLog(taskId);
 }
-
-
