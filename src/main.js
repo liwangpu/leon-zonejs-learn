a5a9ce01022153b31eb564c10ad4f178776cda87
puliwang@cxist.com <puliwang@cxist.com>
2022-06-07 18:16:53 +0800

    test

--- src/main.js
+++ src/main.js
@@ -6,36 +6,43 @@ import { Student } from './student.js';
 const taskId = `task ${+new Date()}`;
 // console.time(taskId);
 
-var myZoneSpec = {
-    name: 'my zone 1',
-    // onHasTask(delegate, current, target, hasTaskState) {
-    //     console.log(hasTaskState.change);          // "macroTask"
-    //     console.log(hasTaskState.macroTask);       // true
-    //     console.log(JSON.stringify(hasTaskState));
-    // },
-    onScheduleTask(delegate, currentZone, targetZone, task) {
-        const result = delegate.scheduleTask(targetZone, task);
-        const name = task.callback.name;
-        console.log(
-            Date.now(),
-            `task with callback '${name}' is added to the task queue`
-        );
-        return result;
-    },
-    onInvokeTask(delegate, currentZone, targetZone, task, ...args) {
-        const result = delegate.invokeTask(targetZone, task, ...args);
-        const name = task.callback.name;
-        console.log(
-            Date.now(),
-            `task with callback '${name}' is removed from the task queue`
-        );
-        return result;
-    }
-};
+function createSpec(name) {
+    return {
+        name,
+        // onHasTask(delegate, current, target, hasTaskState) {
+        //     console.log(hasTaskState.change);          // "macroTask"
+        //     console.log(hasTaskState.macroTask);       // true
+        //     console.log(JSON.stringify(hasTaskState));
+        // },
+        onScheduleTask(delegate, currentZone, targetZone, task) {
+            const result = delegate.scheduleTask(targetZone, task);
+            const name = task.callback.name;
+            console.log(
+                currentZone.name,
+                Date.now(),
+                `task with callback '${name}' is added to the task queue`
+            );
+            return result;
+        },
+        onInvokeTask(delegate, currentZone, targetZone, task, ...args) {
+            const result = delegate.invokeTask(targetZone, task, ...args);
+            const name = task.callback.name;
+            console.log(
+                currentZone.name,
+                Date.now(),
+                `task with callback '${name}' is removed from the task queue`
+            );
+            return result;
+        }
+    };
+}
 
-const myZone = Zone.current.fork(myZoneSpec);
 
-myZone.run(() => {
+
+const z1 = Zone.current.fork(createSpec('z1'));
+const z1_1 = z1.fork(createSpec('z1 - 1'));
+
+z1_1.run(() => {
     const s = new Student('Hellen');
     s.eat();
     s.drink();
